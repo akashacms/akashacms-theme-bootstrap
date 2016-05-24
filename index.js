32508f6dcf30219c8bd24c00e47b5ddd2fd6d28f
David Herron <david@davidherron.com>
2016-05-23 22:49:33 -0700

    Update for akasharender

--- index.js
+++ index.js
@@ -17,49 +17,36 @@
  *  limitations under the License.
  */
 
-var path  = require('path');
-var util  = require('util');
+'use strict';
 
-var akasha;
-var config;
-var logger;
+const path  = require('path');
+const akasha = require('../akasharender');
 
-module.exports.config = function(_akasha, _config) {
-	akasha = _akasha;
-	config = _config;
+const log   = require('debug')('akasha:theme-bootstrap-plugin');
+const error = require('debug')('akasha:error-theme-bootstrap-plugin');
 
-	logger = akasha.getLogger("theme-bootstrap");
+
+module.exports = class ThemeBootstrapPlugin extends akasha.Plugin {
+	constructor() {
+		super("akashacms-theme-bootstrap");
+	}
 	
-    config.root_partials.push(path.join(__dirname, 'partials'));
-    config.root_layouts.push(path.join(__dirname, 'layout'));
-    config.root_assets.unshift(path.join(__dirname, 'bootstrap'));
-    // config.root_assets.push(path.join(__dirname, 'assets'));
-    
-    if (config.headerScripts) {
-        if (!config.themeBootstrap) config.themeBootstrap = {};
-        if (!config.headerScripts.stylesheets) config.headerScripts.stylesheets = [];
-        // Bootstrap 2.3.3
-        // config.headerScripts.stylesheets.unshift({ href: "/bootstrap/css/bootstrap.min.css", media: "screen" });
-        // config.headerScripts.stylesheets.unshift({ href: "/bootstrap/css/bootstrap-responsive.css", media: "screen" });
-        // Bootstrap 3.x
-        config.headerScripts.stylesheets.unshift({
-			href: config.themeBootstrap.bootstrapCSSurl
-                ? config.themeBootstrap.bootstrapCSSurl
-                : "/bootstrap3/css/bootstrap.min.css",
-			/* media: "screen" */
-		});
-        config.headerScripts.stylesheets.unshift({
-			href: config.themeBootstrap.bootstrapThemeCSSurl
-                ? config.themeBootstrap.bootstrapThemeCSSurl
-                : "/bootstrap3/css/bootstrap-theme.min.css",
-			/* media: "screen" */
-		});
+	configure(config) {
+        this._config = config;
+		config.addPartialsDir(path.join(__dirname, 'partials'));
+	}
+
+}
+
+/* -- These are optional addons which work with Bootstrap
+ * -- However, the site configurer can configure these easily
+ * 
 		if (config.themeBootstrap.useHtml5shiv) {
 			config.headerScripts.javaScriptTop.unshift({
 				href: config.themeBootstrap.html5shivUrl
 					? config.themeBootstrap.html5shivUrl
 					: "//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js",
-				/* media: "screen" */
+				/* media: "screen" * /
 			});
 		}
 		if (config.themeBootstrap.useRespondJS) {
@@ -67,48 +54,11 @@ module.exports.config = function(_akasha, _config) {
 				href: config.themeBootstrap.RespondJSUrl
 					? config.themeBootstrap.RespondJSUrl
 					: "//oss.maxcdn.com/respond/1.4.2/respond.min.js",
-				/* media: "screen" */
+				/* media: "screen" * /
 			});
 		}
 
-
-        if (!config.headerScripts.javaScriptBottom)  config.headerScripts.javaScriptBottom = [];
-        // Bootstrap 2.3.3
-        // config.headerScripts.javaScriptBottom.push({ href: "http://code.jquery.com/jquery.js" });
-        // config.headerScripts.javaScriptBottom.push({ href: "/bootstrap/js/bootstrap.min.js" });
-        // Bootstrap 3.x
-        config.headerScripts.javaScriptBottom.unshift({
-			href: config.themeBootstrap.bootstrapJSurl
-                ? config.themeBootstrap.bootstrapJSurl
-                : "/bootstrap3/js/bootstrap.min.js"
-		});
-        config.headerScripts.javaScriptBottom.unshift({
-			href: config.themeBootstrap.jQueryUrl
-                ? config.themeBootstrap.jQueryUrl
-                : "//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"
-		});
-    }
-    
-    // logger.info(util.inspect(config.headerScripts));
-    
-    /* config.funcs.bootstrapDropdown = function(arg, callback) {   
-        var val = akasha.partialSync("bootstrap-dropdown.html.ejs", {
-        	dropdownId: arg.dropdownId
-        });
-        if (callback) callback(undefined, val);
-        return val;
-    } */
-    
-    /* config.funcs.bootstrapPanelHeading = function(arg, callback) {
-        var val = akasha.partialSync("bootstrap-panel-heading.html.ejs", {
-        	header: arg.header
-        });
-        if (callback) callback(undefined, val);
-        return val;
-    } */
-	
-	return module.exports;
-}
+*/
 
 // TBD: HTML filter to change image tags to be responsive http://getbootstrap.com/css/
 
