547c0895e1daf3d3cf130fea9b2ecb940763e693
David Herron <david@davidherron.com>
2021-06-13 17:36:10 -0700

    Update for new FileCache

--- test/index.js
+++ test/index.js
@@ -3,23 +3,50 @@ const akasha   = require('akasharender');
 const plugin = require('../index');
 const { assert } = require('chai');
 
-const config = new akasha.Configuration();
-config.rootURL("https://example.akashacms.com");
-config.configDir = __dirname;
-config.addLayoutsDir('layouts')
-      .addPartialsDir('partials')
-      .addDocumentsDir('documents');
-config.use(plugin);
-config.use(require('akashacms-base'));
-config.setMahabhutaConfig({
-    recognizeSelfClosing: true,
-    recognizeCDATA: true,
-    decodeEntities: true
-});
-config.prepare();
+let config;
 
 
 describe('build site', function() {
+    it('should construct configuration', async function() {
+        config = new akasha.Configuration();
+        config.rootURL("https://example.akashacms.com");
+        config.configDir = __dirname;
+        config.addLayoutsDir('layouts')
+            .addPartialsDir('partials')
+            .addDocumentsDir('documents');
+        config.use(plugin);
+        config.use(require('akashacms-base'));
+        config.setMahabhutaConfig({
+            recognizeSelfClosing: true,
+            recognizeCDATA: true,
+            decodeEntities: true
+        });
+        config.prepare();
+    });
+
+    it('should run setup', async function() {
+        this.timeout(75000);
+        await akasha.cacheSetup(config);
+        await Promise.all([
+            akasha.setupDocuments(config),
+            akasha.setupAssets(config),
+            akasha.setupLayouts(config),
+            akasha.setupPartials(config)
+        ])
+        let filecache = await akasha.filecache;
+        await Promise.all([
+            filecache.documents.isReady(),
+            filecache.assets.isReady(),
+            filecache.layouts.isReady(),
+            filecache.partials.isReady()
+        ]);
+    });
+
+    it('should copy assets', async function() {
+        this.timeout(75000);
+        await config.copyAssets();
+    });
+
     it('should build site', async function() {
         this.timeout(25000);
         let failed = false;
@@ -31,6 +58,11 @@ describe('build site', function() {
             }
         }
         assert.isFalse(failed);
+    });
+
+    it('should close the configuration', async function() {
+        this.timeout(75000);
+        await akasha.closeCaches();
     });
 });
 
